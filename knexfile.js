f81ab2adffb5652311e68deea86da940aee16dfd
Shane <shanelibera7@gmail.com>
2026-04-10 18:02:05 +0100

    chore: deployment fix

--- knexfile.js
+++ knexfile.js
@@ -49,13 +49,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DB_NAME,
-      user:     process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
