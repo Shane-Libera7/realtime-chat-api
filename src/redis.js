a6e4e0408455e05e87366de1f47d39291582cc9c
Shane <shanelibera7@gmail.com>
2026-04-10 18:06:49 +0100

    chore: redis fix

--- src/redis.js
+++ src/redis.js
@@ -1,8 +1,5 @@
 const Redis = require('ioredis');
-const redis = new Redis({
-    host: process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT || 6379
-});
+const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
 
 
 module.exports = redis;
