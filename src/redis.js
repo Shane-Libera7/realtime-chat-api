76aceb9fb98d722696897ecc7fb207bc10fa21e3
Shane <shanelibera7@gmail.com>
2026-04-10 17:15:32 +0100

    chore: docker and deployment

--- src/redis.js
+++ src/redis.js
@@ -1,5 +1,8 @@
 const Redis = require('ioredis');
-const redis = new Redis();
+const redis = new Redis({
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379
+});
 
 
 module.exports = redis;
